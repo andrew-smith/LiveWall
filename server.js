2d10ccb4672e8499ee1cb9940d005aa2eec00c0d
Andrew Smith <andrew@fisherboy.co.nz>
2012-06-13 14:22:18 +1200

    Basic server and logging started

---
+++ server.js
@@ -0,0 +1,13 @@
+
+//--------------------------------------------------------------//main server function
+var log = require('logly');
+var express, //express server instance
+    io;      //socket.io instance
+
+//sets up all functions
+module.exports.init = function(server, socketio, cb)
+{
+    express = server;
+    io = socketio;
+    cb();
+};
