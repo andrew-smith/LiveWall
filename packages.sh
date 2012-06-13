813de00b5cc38064c8bd66a97ff5829e6241565a
Andrew Smith <andrew@fisherboy.co.nz>
2012-06-13 14:21:53 +1200

    Added custom logly module

--- packages.sh
+++ packages.sh
@@ -1,1 +1,3 @@
-npm install hiredis redis express socket.io logly
+npm install hiredis redis express socket.io
+# link to custom logly with colour
+npm install https://github.com/andrew-smith/logly/tarball/master
